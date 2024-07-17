8c56018993c2ac17b7c02046be51d2e852ea400f
kartikey Shahi <kartikeyshahi@hotmail.com>
2024-07-17 14:09:42 +0530

    minor

--- .config/nvim/lua/plugins/none_ls.lua
+++ .config/nvim/lua/plugins/none_ls.lua
@@ -1,14 +1,14 @@
 return {
     "nvimtools/none-ls.nvim",
-    event = "VeryLazy",
+	lazy = false,
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
+			debug = true,
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.djlint,
                 null_ls.builtins.formatting.clang_format,
             },
         })
