17449fc8a01bdbe9594c9ba917a52cb7deb8b182
kartikey Shahi <kartikeyshahi@hotmail.com>
2024-08-17 19:08:40 +0530

    Added some folding config for nvim, and fixed some hyprland scaling

--- .config/nvim/lua/vim-option.lua
+++ .config/nvim/lua/vim-option.lua
@@ -11,8 +11,18 @@ vim.opt.termguicolors = true
 vim.cmd("set path+=**")
 vim.cmd("set wildmenu")
 vim.opt.ignorecase = true
-vim.keymap.set('c', '<tab>', '<C-z>', { silent = false })
+vim.keymap.set("c", "<tab>", "<C-z>", { silent = false })
 vim.cmd("set nohlsearch")
 -- vim.cmd "colorscheme everforest"
 
-
+-- for persistent code folding
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+	pattern = { "*.*" },
+	desc = "save view (folds), when closing file",
+	command = "mkview",
+})
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	pattern = { "*.*" },
+	desc = "load view (folds), when opening file",
+	command = "silent! loadview",
+})
