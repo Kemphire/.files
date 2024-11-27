9a517e166cc1d85317000f4c0077e4e0a61fa029
kartikey Shahi <kartikeyshahi@hotmail.com>
2024-11-27 17:37:00 +0530

    shifted from fzf to telescope, and some minor changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -55,3 +55,12 @@ set -x MANPAGER "nvim +Man!"
 # setting zoxide
 zoxide init fish | source
 
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/hitmonlee/.opam/opam-init/init.fish' && source '/home/hitmonlee/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
