7479fc6ba56f6335b0006ed55a1d87f67188b73d
kartikey Shahi <kartikeyshahi@hotmail.com>
2024-12-09 11:45:02 +0530

    minor changes

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -25,11 +25,11 @@ return {
 		)
 		map("n", "<leader>tf", builtin.treesitter, { noremap = true, silent = true })
 		require("telescope").setup({
-			-- pickers = {
-			find_files = {
-				hidden = true,
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
 			},
-			-- },
 			defaults = {
 				prompt_prefix = "🔍  ",
 				selection_caret = " ",
