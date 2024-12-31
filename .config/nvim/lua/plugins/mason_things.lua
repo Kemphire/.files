d2b8b48f230d88f4db0bc7ab2f59ebce5e33e7a1
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-01-01 01:05:34 +0530

    seperated mason mess

---
+++ .config/nvim/lua/plugins/mason_things.lua
@@ -0,0 +1,40 @@
+	return {
+		{
+		"williamboman/mason.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("mason").setup()
+		end,
+		opts = {
+			ensure_installed = {
+				"clang-format",
+				"latexindent",
+				"biome",
+			},
+		},
+	},
+
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = "VeryLazy",
+		lazy = true,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"quick_lint_js",
+					"clangd",
+					"pyright",
+					"html",
+					"emmet_language_server",
+					"cssls",
+					"texlab",
+					"ruff",
+					"taplo",
+					"biome",
+				},
+				auto_install = true,
+			})
+		end,
+	},
+	}
