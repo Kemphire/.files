5fdcda9cde4d75cd65fcd487c5d636a022ad34af
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-01-29 22:00:42 +0530

    minor changes

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -58,6 +58,24 @@ return {
 				wo = { wrap = true }, -- Wrap notifications
 			},
 		},
+		scroll = {
+			animate = {
+				duration = { step = 15, total = 250 },
+				easing = "linear",
+			},
+			-- faster animation when repeating scroll after delay
+			animate_repeat = {
+				delay = 100, -- delay in ms before using the repeat animation
+				duration = { step = 5, total = 50 },
+				easing = "linear",
+			},
+			-- what buffers to animate
+			filter = function(buf)
+				return vim.g.snacks_scroll ~= false
+					and vim.b[buf].snacks_scroll ~= false
+					and vim.bo[buf].buftype ~= "terminal"
+			end,
+		},
 	},
 	keys = {
 		{
