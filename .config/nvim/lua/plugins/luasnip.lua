60b9a1708b22ecfb272be47572011f1d06ca56e6
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-02-02 22:10:08 +0530

    minor changes

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -4,6 +4,7 @@ return {
 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 	-- install jsregexp (optional!).
 	build = "make install_jsregexp",
+	lazy = true,
 	dependencies = { "rafamadriz/friendly-snippets" },
 
 	config = function()
