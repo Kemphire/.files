f4ab65b9acd2251d6a4bebc1209f0ce2d347d566
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-02-19 16:06:51 +0530

    minor changes

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -63,7 +63,7 @@ return {
 		-- },
 		quickfile = { enabled = true },
 		statuscolumn = { enabled = true },
-		words = { enabled = true },
+		words = { enabled = false },
 		styles = {
 			notification = {
 				wo = { wrap = true }, -- Wrap notifications
