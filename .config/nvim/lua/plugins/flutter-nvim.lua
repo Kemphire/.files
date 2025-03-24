313b1b8ca4e5cda01893ea2c1a0e3c497c052fa4
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-03-24 10:21:33 +0530

    added support for flutter bloc

--- .config/nvim/lua/plugins/flutter-nvim.lua
+++ .config/nvim/lua/plugins/flutter-nvim.lua
@@ -1,10 +1,33 @@
 return {
-	"nvim-flutter/flutter-tools.nvim",
-	lazy = false,
-	ft = "dart",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"stevearc/dressing.nvim", -- optional for vim.ui.select
+	{
+		"nvim-flutter/flutter-tools.nvim",
+		lazy = false,
+		ft = "dart",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim", -- optional for vim.ui.select
+		},
+		config = function()
+			require("flutter-tools").setup({
+				lsp = {
+					color = {
+						enabled = true,
+						background = true,
+						foreground = true,
+					},
+				},
+			}) -- use defaults
+		end,
 	},
-	config = true,
+	{
+		"wa11breaker/flutter-bloc.nvim",
+		dependencies = {
+			"nvimtools/none-ls.nvim", -- Required for code actions
+		},
+		opts = {
+			bloc_type = "default", -- Choose from: 'default', 'equatable', 'freezed'
+			use_sealed_classes = false,
+			enable_code_actions = true,
+		},
+	},
 }
