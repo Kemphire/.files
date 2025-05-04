8f3d7942c89507a9831bd59dedb653bb625b0f27
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-05-05 00:22:18 +0530

    minor change, and fish plugins update

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -96,7 +96,7 @@ local mode = {
 local clients_lsp = function()
 	local bufnr = vim.api.nvim_get_current_buf()
 
-	local clients = vim.lsp.buf_get_clients(bufnr)
+	local clients = vim.lsp.get_clients(bufnr)
 	if next(clients) == nil then
 		return ""
 	end
