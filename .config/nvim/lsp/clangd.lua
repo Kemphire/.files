6bc2f8da4cd1622fc597effdf1986ddbc35c45ac
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-05-15 15:09:27 +0530

    added support for new lsp api, and decreased the reponsibilty of nvim-lspconfig to a descent level

---
+++ .config/nvim/lsp/clangd.lua
@@ -0,0 +1,24 @@
+return {
+	cmd = {
+		"clangd",
+		"--background-index",
+		"--clang-tidy",
+		"--header-insertion=iwyu",
+		"--completion-style=detailed",
+		"--function-arg-placeholders",
+		"--fallback-style=llvm",
+	},
+	init_options = {
+		usePlaceholders = true,
+		completeUnimported = true,
+		clangdFileStatus = true,
+	},
+	filetypes = {
+		"c",
+		"cpp",
+	},
+	root_markers = {
+		"Makefile",
+		"compile_commands.json",
+	},
+}
---
+++ .config/nvim/lsp/gopls.lua
@@ -0,0 +1,12 @@
+return {
+	cmd = { "gopls" },
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+			gofumpt = true,
+		},
+	},
+}
