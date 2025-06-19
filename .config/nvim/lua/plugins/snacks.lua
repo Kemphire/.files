caa6cc183d9c58bda02ec0a7bdfbc2b20d227a3d
Kartikey Shahi <kartikeyshahi@hotmail.com>
2025-06-19 17:05:12 +0530

    made the dashboard a lot simpler

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -10,16 +10,8 @@ return {
 			enabled = true,
 			sections = {
 				{ section = "header" },
-				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "keys",   gap = 1, padding = 1 },
 				{ section = "startup" },
-				{
-					section = "terminal",
-					cmd = "pokemon-colorscripts -r -s --no-title; sleep .1",
-					random = 10,
-					pane = 2,
-					indent = 15,
-					height = 30,
-				},
 			},
 			keys = {
 				{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
@@ -81,8 +73,8 @@ return {
 			-- what buffers to animate
 			filter = function(buf)
 				return vim.g.snacks_scroll ~= false
-					and vim.b[buf].snacks_scroll ~= false
-					and vim.bo[buf].buftype ~= "terminal"
+						and vim.b[buf].snacks_scroll ~= false
+						and vim.bo[buf].buftype ~= "terminal"
 			end,
 		},
 		image = {
@@ -353,12 +345,12 @@ return {
 				Snacks.toggle.diagnostics():map("<leader>ud")
 				Snacks.toggle.line_number():map("<leader>ul")
 				Snacks.toggle
-					.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-					:map("<leader>uc")
+						.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+						:map("<leader>uc")
 				Snacks.toggle.treesitter():map("<leader>uT")
 				Snacks.toggle
-					.option("background", { off = "light", on = "dark", name = "Dark Background" })
-					:map("<leader>ub")
+						.option("background", { off = "light", on = "dark", name = "Dark Background" })
+						:map("<leader>ub")
 				Snacks.toggle.inlay_hints():map("<leader>uh")
 			end,
 		})
