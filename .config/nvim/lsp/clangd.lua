886c26183d32f3f902d00aca39c25816b07b9240
Kartikey Shahi <kartikeyshahi@hotmail.com>
2025-06-20 21:41:30 +0530

    trying ccls

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -1,17 +1,28 @@
 return {
+	-- cmd = {
+	-- 	"clangd",
+	-- 	"--background-index",
+	-- 	"--clang-tidy",
+	-- 	"--header-insertion=iwyu",
+	-- 	"--completion-style=detailed",
+	-- 	"--function-arg-placeholders",
+	-- 	"--fallback-style=llvm",
+	-- },
+	-- init_options = {
+	-- 	usePlaceholders = true,
+	-- 	completeUnimported = true,
+	-- 	clangdFileStatus = true,
+	-- },
+	-- filetypes = {
+	-- 	"c",
+	-- 	"cpp",
+	-- },
+	-- root_markers = {
+	-- 	"Makefile",
+	-- 	"compile_commands.json",
+	-- },
 	cmd = {
-		"clangd",
-		"--background-index",
-		"--clang-tidy",
-		"--header-insertion=iwyu",
-		"--completion-style=detailed",
-		"--function-arg-placeholders",
-		"--fallback-style=llvm",
-	},
-	init_options = {
-		usePlaceholders = true,
-		completeUnimported = true,
-		clangdFileStatus = true,
+		"ccls",
 	},
 	filetypes = {
 		"c",
