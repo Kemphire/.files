784dcc4011f6e370796128e2e985d1e4d66ccab3
Kartikey Shahi <kartikeyshahi@hotmail.com>
2025-07-28 22:55:54 +0530

    minor tweaks

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -10,25 +10,25 @@ map("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
 
 -- for easier motion in bufferline
 
-map(
-	{ "n" },
-	"<leader>h",
-	"<cmd>BufferLineCyclePrev<CR>",
-	{ desc = "Go's ahead in bufferline tabs", noremap = true, silent = true }
-)
-map(
-	{ "n" },
-	"<leader>l",
-	"<cmd>BufferLineCycleNext<CR>",
-	{ desc = "swap buffer in backwards direction", noremap = true, silent = true }
-)
-
-map(
-	{ "n" },
-	"<leader>ch",
-	"<cmd>BufferLinePick<CR>",
-	{ desc = "Toggle picker for bufferlines", noremap = true, silent = true }
-)
+-- map(
+-- 	{ "n" },
+-- 	"<leader>h",
+-- 	"<cmd>BufferLineCyclePrev<CR>",
+-- 	{ desc = "Go's ahead in bufferline tabs", noremap = true, silent = true }
+-- )
+-- map(
+-- 	{ "n" },
+-- 	"<leader>l",
+-- 	"<cmd>BufferLineCycleNext<CR>",
+-- 	{ desc = "swap buffer in backwards direction", noremap = true, silent = true }
+-- )
+--
+-- map(
+-- 	{ "n" },
+-- 	"<leader>ch",
+-- 	"<cmd>BufferLinePick<CR>",
+-- 	{ desc = "Toggle picker for bufferlines", noremap = true, silent = true }
+-- )
 
 map({ "n" }, "<C-s>", "<cmd>wa<CR>", { desc = "Save all the opened buffer", noremap = true, silent = true })
 
--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,15 +1,15 @@
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	event = "VeryLazy",
-	lazy = true,
-	config = function()
-		local diagnostics_provider = "nvim_lsp"
-		require("bufferline").setup({
-			options = {
-				diagnostics = diagnostics_provider,
-			},
-		})
-	end,
+	-- "akinsho/bufferline.nvim",
+	-- version = "*",
+	-- dependencies = "nvim-tree/nvim-web-devicons",
+	-- event = "VeryLazy",
+	-- lazy = true,
+	-- config = function()
+	-- 	local diagnostics_provider = "nvim_lsp"
+	-- 	require("bufferline").setup({
+	-- 		options = {
+	-- 			diagnostics = diagnostics_provider,
+	-- 		},
+	-- 	})
+	-- end,
 }
