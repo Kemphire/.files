11810cb8bb01fffac264abf32b662cd059e04f69
Kartikey Shahi <kartikeyshahi@hotmail.com>
2025-12-17 12:05:03 +0530

    python lsp to ty

---
+++ .config/nvim/lsp/pyrefly.lua
@@ -0,0 +1,5 @@
+return {
+	-- cmd = { "pyrefly", "lsp" },
+	-- filetypes = { "python" },
+	-- root_markers = { "pyrefly.toml", "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+}
